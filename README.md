a86ec7b23749c7a2118b55f9240cc7ade7d5c018
Shepherd23333 <927561263l@gmail.com>
2024-11-11 15:00:49 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+https://poi.apache.org/apidocs/5.0/  
+https://blog.csdn.net/csdnerM/article/details/139303495
---
+++ src/main/java/me/shepherd23333/Main.java
@@ -0,0 +1,15 @@
+package me.shepherd23333;
+
+import me.shepherd23333.file.test;
+
+//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
+// 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
+public class Main {
+    public static void main(String[] args) {
+        try {
+            test.create();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
